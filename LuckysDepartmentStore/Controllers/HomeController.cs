4ac3f844ef8b62a5c1ddddfc3550774f1542015b
Troy Gugler <guglert80@gmail.com>
2024-10-29 11:28:20 -0500

    add images, complete _layout.

--- LuckysDepartmentStore/Controllers/HomeController.cs
+++ LuckysDepartmentStore/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace LuckysDepartmentStore.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpPost]
+        public IActionResult Search(string searchString)
+        {
+            return View();
+        }
     }
 }
